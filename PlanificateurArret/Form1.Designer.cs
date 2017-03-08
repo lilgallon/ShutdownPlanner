754e00ea6c5b9fb66f05216cfaf5410f8e3f5663
Lilian Gallon <lgallon@IUT.BX1>
2017-03-08 20:18:41 +0100

    V1 - Restart & Shutdown

--- PlanificateurArret/Form1.Designer.cs
+++ PlanificateurArret/Form1.Designer.cs
@@ -30,17 +30,17 @@
         {
             this.timePicker = new System.Windows.Forms.DateTimePicker();
             this.planningButton = new System.Windows.Forms.Button();
+            this.checkBoxRestart = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // timePicker
             // 
             this.timePicker.CustomFormat = "H:mm";
             this.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.timePicker.Location = new System.Drawing.Point(88, 97);
+            this.timePicker.Location = new System.Drawing.Point(101, 88);
             this.timePicker.Name = "timePicker";
-            this.timePicker.Size = new System.Drawing.Size(94, 20);
+            this.timePicker.Size = new System.Drawing.Size(81, 20);
             this.timePicker.TabIndex = 0;
-            this.timePicker.ValueChanged += new System.EventHandler(this.timePicker_ValueChanged);
             // 
             // planningButton
             // 
@@ -52,23 +52,35 @@
             this.planningButton.UseVisualStyleBackColor = true;
             this.planningButton.Click += new System.EventHandler(this.planningButton_Click);
             // 
+            // checkBoxRestart
+            // 
+            this.checkBoxRestart.AutoSize = true;
+            this.checkBoxRestart.Location = new System.Drawing.Point(101, 123);
+            this.checkBoxRestart.Name = "checkBoxRestart";
+            this.checkBoxRestart.Size = new System.Drawing.Size(81, 17);
+            this.checkBoxRestart.TabIndex = 2;
+            this.checkBoxRestart.Text = "Redémarrer";
+            this.checkBoxRestart.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.checkBoxRestart);
             this.Controls.Add(this.planningButton);
             this.Controls.Add(this.timePicker);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DateTimePicker timePicker;
         private System.Windows.Forms.Button planningButton;
+        private System.Windows.Forms.DateTimePicker timePicker;
+        private System.Windows.Forms.CheckBox checkBoxRestart;
     }
 }
 
--- PlanificateurArret/Form1.cs
+++ PlanificateurArret/Form1.cs
@@ -1,40 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace PlanificateurArret
 {
     public partial class Form1 : Form
     {
+        private int minutes;
+        private int hour;
+        private int seconds;
+        private string secondsStr;
+        private string commandShutdown;
+        private string commandRestart;
         public Form1()
         {
             InitializeComponent();
-            InitializeTimePicker();
-        }
-
-        private void InitializeTimePicker()
-        {
-            timePicker = new DateTimePicker();
-            timePicker.Format = DateTimePickerFormat.Custom;
-            timePicker.CustomFormat = "HH:mm"; // Only use hours and minutes
-            timePicker.ShowUpDown = true;
         }
 
         private void planningButton_Click(object sender, EventArgs e)
         {
+
+            minutes = timePicker.Value.Minute;
+            hour = timePicker.Value.Hour;
+            seconds = minutes * 60 + hour * 3600;
+            secondsStr = seconds.ToString();
+            commandShutdown = "shutdown -s -t " + secondsStr;
+            commandRestart = "shutdown -a -t " + secondsStr;
+
+            // Console
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            if (checkBoxRestart.Checked)
+            {
+                cmd.StandardInput.WriteLine(commandRestart);
+            }
+            else
+            {
+                cmd.StandardInput.WriteLine(commandShutdown);
+            }
+      
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+
+            // System.Diagnostics.Process.Start("CMD.exe", secondsStr);
             // envoyer la planif
         }
 
-        private void timePicker_ValueChanged(object sender, EventArgs e)
-        {
-            string date;
-            this.timePicker;
-        }
     }
 }
